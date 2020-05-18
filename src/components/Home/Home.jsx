174912ee900def11c9d54241b7f3247a030d720b
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-18 18:04:58 +0200

    First page home almost done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactFullpage from '@fullpage/react-fullpage';
 import './Home.css';
+import Logo from '../../Assets/img/logo_pokewiki.png';
+import Arrow from '../../Assets/down-arrow.svg';
 import Menu from '../Menu/Menu';
 
 
@@ -8,21 +10,25 @@ export default class Home extends Component {
     render() {
         return (
             <ReactFullpage
-                //fullpage options
-                licenseKey={'YOUR_KEY_HERE'}
-                scrollingSpeed={1000} /* Options here */
-
+                scrollingSpeed={1000}
                 render={({ state, fullpageApi }) => {
                     return (
                         <ReactFullpage.Wrapper>
                             <div className="section homepage">
-                                <p>PokéWiki</p>
-                                <button onClick={() => fullpageApi.moveSectionDown()}>
-                                    Click me to move down
-            </button>
+                                <section className="main_logo">
+                                    <img src={Logo} className="img_logo" alt="" />
+                                    <h1 className="title">PokéWiki</h1>
+                                </section>
+                                <img src={Arrow} alt="" className="arrow_down" onClick={() => fullpageApi.moveSectionDown()}/>
                             </div>
                             <div className="section">
                                 <h1>Pokémon</h1>
+                            </div>
+                            <div className="section">
+                                <h1>Movimientos</h1>
+                            </div>
+                            <div className="section">
+                                <h1>Tipos</h1>
                             </div>
                         </ReactFullpage.Wrapper>
                     );
