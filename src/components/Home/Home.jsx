ec46813f766b5d0ac98dcb2acf8cea100d363947
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-19 18:02:23 +0200

    Responsive for first section of homepage is done.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,20 +8,22 @@ export default class Home extends Component {
     render() {
         return (
             <ReactFullpage
-            //Se puede usar gratuitamente, pero si no tienes licencia comprada, saldrá un "error" en la consola.
-                licenseKey = {'OPEN-SOURCE-GPLV3-LICENSE'}
+                //Se puede usar gratuitamente, pero si no tienes licencia comprada, saldrá un "error" en la consola.
+                licenseKey={'OPEN-SOURCE-GPLV3-LICENSE'}
                 scrollingSpeed={1000}
                 render={({ state, fullpageApi }) => {
                     return (
                         <ReactFullpage.Wrapper>
                             <div className="section homepage">
-                                <section className="main_logo">
-                                    <img src={Logo} className="img_logo" alt="" />
-                                    <h1 className="title">PokéWiki</h1>
-                                </section>
-                                <p>Bienvenidos a PokéWiki, una enciclopedia online dedicada al universo Pokémon. En ella encontrarás información detallada de cada Pokémon, de los tipos y de los movimientos.</p>
-                                <img src={Arrow} alt="Ir abajo automáticamente" tabIndex="0"
-                                    role="button" className="arrow_down" onClick={() => fullpageApi.moveSectionDown()} />
+                                <div className="content_container">
+                                    <section className="main_logo">
+                                        <img src={Logo} className="img_logo" alt="" />
+                                        <h1 className="title">PokéWiki</h1>
+                                    </section>
+                                    <p>Bienvenidos a PokéWiki, una enciclopedia online dedicada al universo Pokémon. En ella encontrarás información detallada de cada Pokémon, de los tipos y de los movimientos.</p>
+                                    <img src={Arrow} alt="Ir abajo automáticamente" tabIndex="0"
+                                        role="button" className="arrow_down" onClick={() => fullpageApi.moveSectionDown()} />
+                                </div>
                             </div>
                             <div className="section">
                                 <h1>Pokémon</h1>
