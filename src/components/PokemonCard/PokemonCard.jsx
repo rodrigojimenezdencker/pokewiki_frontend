24d2256e8ade1643053704b9a297984efcb28736
Guillem Ramoneda <guillemramonedabejar@gmail.com>
2020-05-19 20:03:45 +0200

    Cards error on null

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -4,8 +4,12 @@ export default class PokemonCard extends Component {
     render() {
         return (
             <div>
-                <p>{this.props.name}</p>
-                <img src={this.props.image} alt=""/>
+                <p>#{this.props.numPokedex} - {this.props.name}</p>
+                <img src={this.props.image} />
+                <img src={this.props.type1} />
+                <p>COLOR: {this.props.type1Color}</p>
+                <img src={this.props.type2} />
+                <p>COLOR: {this.props.type2Color}</p>
             </div>
         )
     }
