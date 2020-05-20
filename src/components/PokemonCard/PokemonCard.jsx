36ba3462259395cf53ec8ab38f57205746ff165e
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-20 20:15:38 +0200

    Added routing in pokemon list

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class PokemonCard extends Component {
     render() {
         return (
-            <div>
-                <p>#{this.props.numPokedex} - {this.props.name}</p>
-                <img src={this.props.image} />
-                <img src={this.props.type1} />
-                <p>{this.props.type1Color}</p>
-                <img src={this.props.type2} />
-                <p>{this.props.type2Color}</p>
-            </div>
+            <Link to={`/pokemon/${this.props.numPokedex}`}>
+                <div>
+                    <p>#{this.props.numPokedex} - {this.props.name}</p>
+                    <img src={this.props.image} />
+                    <img src={this.props.type1} />
+                    <p>{this.props.type1Color}</p>
+                    <img src={this.props.type2} />
+                    <p>{this.props.type2Color}</p>
+                </div>
+            </Link>
         )
     }
 }
