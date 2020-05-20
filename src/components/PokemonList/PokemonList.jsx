c5ae04043ee7b2689e05c3fd313b17fd5d5f660f
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-20 20:22:08 +0200

    Idented code

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PokemonCard from '../PokemonCard/PokemonCard';
 
 export default class PokemonList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             pokemonlist: []
@@ -11,28 +11,28 @@ export default class PokemonList extends Component {
 
     componentDidMount() {
         fetch('https://localhost:44316/api/pokemon')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({pokemonlist: data});
-            console.log(this.state.pokemonlist);
-        } )
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ pokemonlist: data });
+                console.log(this.state.pokemonlist);
+            })
     }
     render() {
         return (
             <div>
                 {this.state.pokemonlist.map(item => {
                     return (
-                    <PokemonCard 
-                        numPokedex={item.numPokedex} 
-                        name={item.name} 
-                        image={item.image} 
-                        type1Image={item.type1.image} 
-                        type1Color={item.type1.color} 
-                        type1Name={item.type1.name}
-                        type2Image={item.type2 == null ? item.type2 : item.type2.image} 
-                        type2Color={item.type2 == null ? item.type2 : item.type2.color}
-                        type2Name={item.type2 == null ? item.type2 : item.type2.name}
-                    />
+                        <PokemonCard
+                            numPokedex={item.numPokedex}
+                            name={item.name}
+                            image={item.image}
+                            type1Image={item.type1.image}
+                            type1Color={item.type1.color}
+                            type1Name={item.type1.name}
+                            type2Image={item.type2 == null ? item.type2 : item.type2.image}
+                            type2Color={item.type2 == null ? item.type2 : item.type2.color}
+                            type2Name={item.type2 == null ? item.type2 : item.type2.name}
+                        />
                     )
                 })}
             </div>
