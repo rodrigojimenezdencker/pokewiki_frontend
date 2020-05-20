e69b70dae8fb7c7db1369aa152f3946fad2329fa
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-21 01:27:10 +0200

    Added some styles to PokemonList

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PokemonCard from '../PokemonCard/PokemonCard';
+import './PokemonList.css';
 
 export default class PokemonList extends Component {
     constructor(props) {
@@ -19,23 +20,25 @@ export default class PokemonList extends Component {
     }
     render() {
         return (
-            <div>
-                {this.state.pokemonlist.map(item => {
-                    return (
-                        <PokemonCard
-                            numPokedex={item.numPokedex}
-                            name={item.name}
-                            image={item.image}
-                            type1Image={item.type1.image}
-                            type1Color={item.type1.color}
-                            type1Name={item.type1.name}
-                            type2Image={item.type2 == null ? item.type2 : item.type2.image}
-                            type2Color={item.type2 == null ? item.type2 : item.type2.color}
-                            type2Name={item.type2 == null ? item.type2 : item.type2.name}
-                        />
-                    )
-                })}
-            </div>
+            <section id="pokemonlist_page">
+                <div className="pokemonlist_grid">
+                    {this.state.pokemonlist.map(item => {
+                        return (
+                            <PokemonCard
+                                numPokedex={item.numPokedex}
+                                name={item.name}
+                                image={item.image}
+                                type1Image={item.type1.image}
+                                type1Color={item.type1.color}
+                                type1Name={item.type1.name}
+                                type2Image={item.type2 == null ? item.type2 : item.type2.image}
+                                type2Color={item.type2 == null ? item.type2 : item.type2.color}
+                                type2Name={item.type2 == null ? item.type2 : item.type2.name}
+                            />
+                        )
+                    })}
+                </div>
+            </section>
         )
     }
 }
