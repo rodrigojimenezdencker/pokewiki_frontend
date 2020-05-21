b8a1f27f0593065743892d743306eb1dc5b4b9bb
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-21 18:59:08 +0200

    Deleted test class

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -4,7 +4,7 @@ import './PokemonCard.css';
 export default class PokemonCard extends Component {
     render() {
         return (
-            <div className={`card test type${this.props.type1Id}`}>
+            <div className={`card type${this.props.type1Id}`}>
                 <p className="numpokedex">#{this.props.numPokedex}</p>
                 <img src={this.props.image} alt={this.props.name} className="img_pokemon" />
                 <div className="name-type_container">
