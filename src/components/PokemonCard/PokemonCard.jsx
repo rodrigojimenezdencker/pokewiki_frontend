a61bd3eac688179781cde62e165c3488367cfff6
Rodrigo Fernando Jiménez Dencker <rodrigo.jimenez.developer@gmail.com>
2020-05-21 21:55:56 +0200

    Import of all images correctly

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -3,14 +3,22 @@ import './PokemonCard.css';
 
 export default class PokemonCard extends Component {
     render() {
+        function importAll(r) {
+            let images = {};
+            r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+            return images;
+        }
+        
+        const pokemonImages = importAll(require.context('../../Assets/img/pokemon', false, /\.(png)$/));
+        
         return (
             <div className={`card type${this.props.type1Id}`}>
                 <p className="numpokedex">#{this.props.numPokedex}</p>
-                <img src={this.props.image} alt={this.props.name} className="img_pokemon" />
+                <img src={pokemonImages[this.props.image]} alt={this.props.name} className="img_pokemon" />
                 <div className="name-type_container">
                     <p>{this.props.name}</p>
                     <img src={this.props.type1Image} alt={this.props.type1Name} className="type_image" />
-                    <img src={this.props.type2Image} alt={this.props.type2Name} className="type_image"/>
+                    <img src={this.props.type2Image} alt={this.props.type2Name} className="type_image" />
                 </div>
             </div>
         )
