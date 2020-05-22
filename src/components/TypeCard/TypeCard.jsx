3de02e22f50efeab955a31c768161efa0f3fb9a1
Guillem Ramoneda <guillemramonedabejar@gmail.com>
2020-05-22 18:07:15 +0200

    Added svg for types and css with jsx for the icons

--- src/components/TypeCard/TypeCard.jsx
+++ src/components/TypeCard/TypeCard.jsx
@@ -3,12 +3,22 @@ import './TypeCard.css';
 
 export default class TypeCard extends Component {
     render() {
+        function importAll(r) {
+            let images = {};
+            r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+            return images;
+        }
+        
+        const typesImages = importAll(require.context('../../Assets/svg/types', false, /\.(svg)$/));
+
         return (
             <div>
                 <p>{this.props.typeId}</p>
                 <p>{this.props.name}</p>
                 <p>{this.props.color}</p>
-                <img src={this.props.image} />
+                <div className={`icon typeImage${this.props.typeId}`}>
+                    <img src={typesImages[this.props.image]} />
+                </div>
                 <img src={this.props.secondaryImage} />
             </div>
         )
