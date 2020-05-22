d495c376be74e28235078d707d875d19d5a8cae3
Guillem Ramoneda <guillemramonedabejar@gmail.com>
2020-05-22 22:27:39 +0200

    Inputs done

--- src/components/CRUD/Create/Create.jsx
+++ src/components/CRUD/Create/Create.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Input } from 'reactstrap';
+import { Input, Form, FormGroup, Label } from 'reactstrap';
 
 export default class Create extends Component {
     constructor(props) {
@@ -61,37 +61,190 @@ export default class Create extends Component {
     }
 
     render() {
-        console.log(this.state.name)
         return (
-            <form>
-                <Input
-                    type="number"
-                    name="numPokedex"
-                    onChange={this.handleChangeInputNumber}
-                    placeholder="Inserta numPokédex"
-                />
-                <input onChange={this.handleChangeInputNumber} type="number" name="numPokedex" />
-                <input onChange={this.handleChangeInput} type="text" name="name" />
-                <input onChange={this.handleChangeInput} type="text" name="description" />
-                <input onChange={this.handleChangeInput} type="text" name="ability" />
-                <input onChange={this.handleChangeInput} type="text" name="secondaryAbility" />
-                <input onChange={this.handleChangeInput} type="text" name="hnamedenAbility" />
-                <input onChange={this.handleChangeInput} type="text" name="image" />
-                <input onChange={this.handleChangeInput} type="text" name="weight" />
-                <input onChange={this.handleChangeInput} type="text" name="height" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="ps" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="attack" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="defense" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="spAttack" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="spDefense" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="speed" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="prevolution" />
-                <input onChange={this.handleChangeInputNumber} type="number" name="evolution" />
-                <input onChange={this.handleChangeInput} type="text" name="evolutionRequirements" />
+            <Form>
+                <FormGroup>
+                    <Label for="numPokedex">Num. Pokédex</Label>
+                    <Input
+                        type="number"
+                        id="numPokedex"
+                        name="numPokedex"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta el número de la Pokédex"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="name">Nombre</Label>
+                    <Input
+                        type="text"
+                        id="name"
+                        name="name"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta el nombre del Pokémon"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="description">Descricpión</Label>
+                    <Input
+                        type="textarea"
+                        id="description"
+                        name="description"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la descripción"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="ability">Habilidad</Label>
+                    <Input
+                        type="text"
+                        id="ability"
+                        name="ability"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la habilidad"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="secondaryAbility">Habilidad secundaria</Label>
+                    <Input
+                        type="text"
+                        id="secondaryAbility"
+                        name="secondaryAbility"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la habilidad secundaria"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="hiddenAbility">Habilidad oculta</Label>
+                    <Input
+                        type="text"
+                        id="hiddenAbility"
+                        name="hiddenAbility"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la habilidad oculta"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="image">Imagen</Label>
+                    <Input
+                        type="text"
+                        id="image"
+                        name="image"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la imagen"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="weight">Peso</Label>
+                    <Input
+                        type="text"
+                        id="weight"
+                        name="weight"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta el peso del Pokémon"
+                        required
+                    />
+                    <Label for="height">Altura</Label>
+                    <Input
+                        type="text"
+                        id="height"
+                        name="height"
+                        onChange={this.handleChangeInput}
+                        placeholder="Inserta la altura del Pokémon"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="ps">PS</Label>
+                    <Input
+                        type="number"
+                        id="ps"
+                        name="ps"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta la vida del Pokémon"
+                        required
+                    />
+                    <Label for="attack">Ataque</Label>
+                    <Input
+                        type="number"
+                        id="attack"
+                        name="attack"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta el ataque del Pokémon"
+                        required
+                    />
+                    <Label for="defense">Defensa</Label>
+                    <Input
+                        type="number"
+                        id="defense"
+                        name="defense"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta la defensa del Pokémon"
+                        required
+                    />
+                    <Label for="spAttack">Ataque especial</Label>
+                    <Input
+                        type="number"
+                        id="spAttack"
+                        name="spAttack"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta el ataque especial del Pokémon"
+                        required
+                    />
+                    <Label for="spDefense">Defensa Especial</Label>
+                    <Input
+                        type="number"
+                        id="spDefense"
+                        name="spDefense"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta la defensa especial del Pokémon"
+                        required
+                    />
+                    <Label for="speed">Velocidad</Label>
+                    <Input
+                        type="number"
+                        id="speed"
+                        name="speed"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta la velocidad del Pokémon"
+                        required
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="prevolution">Preevolución</Label>
+                    <Input
+                        type="number"
+                        id="prevolution"
+                        name="prevolution"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta la preevolución del Pokémon"
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="evolution">Evolución</Label>
+                    <Input
+                        type="number"
+                        id="evolution"
+                        name="evolution"
+                        onChange={this.handleChangeInputNumber}
+                        placeholder="Inserta  del Pokémon"
+                    />
+                    <Label for="evolutionRequirements">Requerimiento para Evolucionar</Label>
+                    <Input
+                        type="text"
+                        id="evolutionRequirements"
+                        name="evolutionRequirements"
+                        onChange={this.handleChangeInput}
+                        placeholder="Requerimiento para evolucionar Pokémon"
+                    />
+                </FormGroup>
                 <input type="button" onClick={this.enviar} value="Enviar" />
                 <input type="button" onClick={() => console.log(JSON.stringify(this.state))} value="ESTADO" />
-
-            </form>
+            </Form>
         )
     }
 }
